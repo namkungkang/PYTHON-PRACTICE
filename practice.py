152d04a06c8b2f3ff0cd56299d764da5cbc965dc
namkungkang <139133085+namkungkang@users.noreply.github.com>
2023-08-28 22:33:06 +0900

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,88 @@
+#숫자형 자료형
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*-3)
+print(3*(3+1))
+#문자열 자료형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*9)
+# boolean자료형
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not False)
+print(not True)
+print(not(5>10))
+
+#변수
+animal = '고양이'
+name = '해피'
+age = 4
+hobby = '낮잠'
+is_adult = age >= 3
+
+
+
+
+print("우리집 " + animal +" 의 이름은 " +name+"에요")
+hobby = '공놀이'
+print(name, "는", age, "살이며 ", hobby, "을 아주 좋아해요")
+print(name, "는  어른일까요?"  ,is_adult)
+
+station1="사당"
+station2="신도림"
+station3="인천공항"
+
+print(station3 + "행 열차가 들어오고 있습니다")
+
+#연산자
+print(1+1)
+print(2**2)
+print(5%3)
+print(10%3)
+print(5//3)
+print(10//3)
+
+print(10 > 3)
+print(4 >= 7)
+print(10 < 3)
+print(5 >= 5)
+
+print(3==3) 
+print(4==2)
+print(3+4 == 7)
+print((4*5) / 2 ==10)
+print(1!=3)
+print(not 3!= 5)
+print((4>3) and (5>7))
+print((4>5) & (5>3))
+print((5>5) or (5>3))
+print(5>3>5)
+
+#수식
+print(2+3*4)
+print((5+3) * 4)
+number = 4+3*2
+print(number)
+number = number + 2
+print(number)
+number += 2
+print(number)
+number *=2
+print(number)
+number /= 2
+print(number)
+number-=2
+print(number)
+number //=2
+print(number)
+number **=5
+print(number)
+number %=5
+print(number)
