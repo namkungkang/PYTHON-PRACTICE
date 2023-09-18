559fbee4459a720198127a6e720d3576682f4fcc
namkungkang <139133085+namkungkang@users.noreply.github.com>
2023-09-18 21:24:30 +0900

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,49 @@
+# #1번
+# x = int(input('x 입력 : '))
+# y = int(input('y 입력 : '))
+# print('두수의 합 ', x+y)
+# print('두수의 차 ', x-y)
+# print('두수의 곱 ', x*y)
+# print('두수의 평균 ', (x+y)/2)
+# if(x>y) :
+#     print('큰 수 :' ,x)
+# elif(x<y) :
+#     print('큰 수 :' ,y)
+# else :
+#     print('값이 같다' )
+
+# if(x>y) :
+#     print('작은 수 :' ,y)
+# elif(x<y) :
+#     print('작은 수 :' ,x)
+# else :
+#     print('값이 같다' )
+    
+#2번   
+# x = int(input('닭의 수 : '))
+# y = int(input('돼지의 수 : '))
+# z = int(input('소의 수 : '))
+# print('전체 다리의 수 :' ,(x*2)+(y*4)+(z*4))
+
+#3번
+# temp1 = int(input('시간을 입력하시오 : '))
+# temp2 = int(input('분을 입력하시오 : '))
+# x = temp1 * 3600 
+# y = temp2 * 60
+# z = x+y
+# print('{0}시간 {1}분은 {2}초 입니다 '.format(temp1,temp2,z))
+
+# 4번
+# import math
+# x1 = int(input('x1 :'))
+# y1= int(input('y1 :'))
+# x2 = int(input('x2 :'))
+# y2 = int(input('y2 :'))
+
+# print('두점 사이의 거리 = ' , (math.sqrt((x2-x1)**2 + (y2-y1)**2)))
+
+#5번
+# temp = (input('세 자리 정수를 입력하세요 :'))
+# print('백의 자리 :' + temp[0])
+# print('십의 자리 :' +temp[1])
+# print('일의 자리 :' +temp[2])
